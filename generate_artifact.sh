c9136913e2bfa5bf1e2c90b73812352274379008
Raul Velaz <raul@radixdlt.com>
2022-01-12 12:39:19 +0100

    do not generate archive node artifacts (#87)
    * do not generate archive node artifacts

--- generate_artifact.sh
+++ generate_artifact.sh
@@ -10,28 +10,14 @@ export NGINX_CLIENT_HTTP_PORT=8080
 export NGINX_NODE_HTTP_PORT=3333
 
 #Remove the nginx configuration part and generate nginx.conf
-for node_type in full_node archive_node;do
-  if [[ "$node_type" == "full_node" ]]; then
-    nginx_file_name="nginx-fullnode.conf"
-  elif [[ "$node_type" == "archive_node" ]]; then
-    export RADIXDLT_ARCHIVE_API_ENABLE=true
-    export RADIXDLT_CONSTRUCTION_API_ENABLE=true
-    export RADIXDLT_TRANSACTIONS_API_ENABLE=true
-    nginx_file_name="nginx-archive.conf"
-  fi
-  sed '1,/# nginx configuration/!d' docker-entrypoint.sh  | sed 's/\/etc\/nginx/${PWD}/g' | bash
-  mv ${PWD}/nginx.conf ${PWD}/$nginx_file_name
-  #Remove nginx user
-  sed -i "s|user nginx;|include \/etc\/nginx\/modules-enabled\/*.conf;|g" ${PWD}/$nginx_file_name
-  #Change /dev/stdout
-  sed -i "s|\/dev\/stdout|\/var\/log\/nginx\/access.log|g" ${PWD}/$nginx_file_name
-
-  if [[ "$node_type" == "full_node" ]]; then
-    zip -r radixdlt-nginx-fullnode-conf.zip conf.d/ nginx-fullnode.conf
-  elif [[ "$node_type" == "archive_node" ]]; then
-    zip -r radixdlt-nginx-archive-conf.zip conf.d/ nginx-archive.conf
-  fi
-done
+nginx_file_name="nginx-fullnode.conf"
+sed '1,/# nginx configuration/!d' docker-entrypoint.sh  | sed 's/\/etc\/nginx/${PWD}/g' | bash
+mv ${PWD}/nginx.conf ${PWD}/$nginx_file_name
+#Remove nginx user
+sed -i "s|user nginx;|include \/etc\/nginx\/modules-enabled\/*.conf;|g" ${PWD}/$nginx_file_name
+#Change /dev/stdout
+sed -i "s|\/dev\/stdout|\/var\/log\/nginx\/access.log|g" ${PWD}/$nginx_file_name
+zip -r radixdlt-nginx-fullnode-conf.zip conf.d/ nginx-fullnode.conf
 #Cleanup
-rm nginx-archive.conf nginx-fullnode.conf
+rm nginx-fullnode.conf
 
